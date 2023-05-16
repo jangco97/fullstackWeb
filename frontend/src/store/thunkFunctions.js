6ca89f0590e09f258a6d53f66df9fb744a4ff417
geon woo <qkrnslehd@naver.com>
2023-05-16 14:01:38 +0900

    feat: add thunkFunctions.js for store

--- frontend/src/store/thunkFunctions.js
+++ frontend/src/store/thunkFunctions.js
@@ -43,3 +43,14 @@ export const logoutUser = createAsyncThunk('user/logoutUser', async (_, thunkAPI
     return thunkAPI.rejectWithValue(error.response.data || error.message);
   }
 });
+
+export const addToCart = createAsyncThunk('user/addToCart', async (body, thunkAPI) => {
+  try {
+    const response = await axiosInstance.post(`/users/cart`, body);
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return thunkAPI.rejectWithValue(error.response.data || error.message);
+  }
+});
